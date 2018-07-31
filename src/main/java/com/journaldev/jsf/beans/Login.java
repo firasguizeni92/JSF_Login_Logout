c2f0bdd71eae5deeff65b6a18c4bca12d61abc1d
Firas Guizeni <firasguizeni9292@gmail.com>
2018-07-31 12:21:08 +0100

    first commit

---
+++ src/main/java/com/journaldev/jsf/beans/Login.java
@@ -0,0 +1,114 @@
+package com.journaldev.jsf.beans;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.journaldev.jsf.dao.LoginDAO;
+import com.journaldev.jsf.util.SessionUtils;
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+	
+	private String pwd;
+	private String msg;
+	private String user;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	
+	
+//	public void test1() {
+//		try {
+//		WebDriver driver ;
+//		driver = new ChromeDriver();
+//		driver.findElement(By.id("username")).sendKeys("adam");
+//		driver.findElement(By.id("password")).sendKeys("adam");
+//		Thread.sleep(3000);
+//		driver.findElement(By.id("bouton")).click();
+//	} catch (InterruptedException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	
+//	}
+		
+		
+		
+	
+	
+	
+	//validate login
+	public String validateUsernamePassword() {
+//		test();
+		boolean valid = LoginDAO.validate(user, pwd);
+		if (valid) {
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", user);
+			return "admin";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Incorrect Username and Passowrd",
+							"Please enter correct username and Password"));
+			return "login";
+		}
+	}
+
+	//logout event, invalidate session
+	public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login";
+	}
+	
+	
+	
+	
+//	public static void main(String[] args ) {
+//		Login a = new Login();
+//		a.test1();
+//	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/journaldev/jsf/dao/LoginDAO.java
@@ -0,0 +1,82 @@
+package com.journaldev.jsf.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.journaldev.jsf.util.DataConnect;
+
+public class LoginDAO {
+	
+//	public static void test1() {
+//		try {
+//		WebDriver driver ;
+//	System.setProperty("webdriver.chrome.driver", "C:\\developpement\\chromedriver.exe");
+//
+//		driver = new ChromeDriver();
+//		driver.get("http://localhost:8080/JSF_Login_Logout/faces/login.xhtml");
+//		driver.manage().window().maximize();
+//		driver.findElement(By.id("username")).sendKeys("adam");
+//		driver.findElement(By.id("password")).sendKeys("adam");
+//		Thread.sleep(3000);
+//		driver.findElement(By.id("bouton")).click();
+//	} catch (InterruptedException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	
+//	}
+	
+	
+
+	public static boolean validate(String user, String pwd) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		try {
+			con = DataConnect.getConnection();
+			
+			
+			
+			
+			
+			ps = con.prepareStatement("Select uname, password from users where uname = ? and password = ?");
+			
+
+		
+			ps.setString(1, user);
+		ps.setString(2, pwd);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				return true;
+			}
+		} catch (SQLException ex) {
+			System.out.println("Login error -->" + ex.getMessage());
+			return false;
+		} finally {
+			DataConnect.close(con);
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+}
